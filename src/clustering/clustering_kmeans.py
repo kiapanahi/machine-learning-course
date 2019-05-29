b67f3b67f0fedc69e630c3374311611831eaa3bc
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-05-29 17:55:06 +0430

    Draw kmeans inertias w/ different cluster numbers

--- src/clustering/clustering_kmeans.py
+++ src/clustering/clustering_kmeans.py
@@ -16,19 +16,30 @@ blob_std = np.array([.4, .3, .1, .1, .1])
 X, y = make_blobs(n_samples=2000, centers=blob_centers, cluster_std=blob_std)
 
 if __name__ == "__main__":
-    kmeans = KMeans(n_clusters=5)
-    kmeans.fit(X)
-    print('labels: {}'.format(kmeans.labels_))
-    print('inertia: {}'.format(kmeans.inertia_))
-    print('centers: {}'.format(kmeans.cluster_centers_))
 
-    vr.plot_decision_boundaries(kmeans, X,
-                                resolution=1000,
-                                show_centroids=True,
-                                show_xlabels=True,
-                                show_ylabels=True)
+    # plt.scatter(X[:, 0], X[:, 1], s=5, alpha=.7, c=y)
 
+    kmeans_inertia = []
+    for i in range(2, 10):
+        kmeans = KMeans(n_clusters=i)
+        kmeans.fit(X)
+        print('labels: {}'.format(kmeans.labels_))
+        print('inertia: {}'.format(kmeans.inertia_))
+        print('centers: {}'.format(kmeans.cluster_centers_))
+        kmeans_inertia.append([i, kmeans.inertia_])
+
+    kmeans_inertia = np.c_[kmeans_inertia]
+    print(kmeans_inertia)
+    plt.plot(kmeans_inertia[:, 0], kmeans_inertia[:, 1])
     plt.show()
+
+    # vr.plot_decision_boundaries(kmeans, X,
+    #                             resolution=1000,
+    #                             show_centroids=True,
+    #                             show_xlabels=True,
+    #                             show_ylabels=True)
+
+    # plt.show()
 
     # plt.scatter(X[:, 0], X[:, 1], s=5, alpha=.7, c=y)
     # plt.scatter(X[:, 0], X[:, 1], s=5, alpha=.7, c=kmeans.labels_)
