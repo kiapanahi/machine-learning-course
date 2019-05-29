9f8f1730cc424a9dac5db92461a8fa4852d1f3de
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-05-29 12:07:34 +0430

    Add session 9 files

---
+++ src/9/silhouette_diag.py
@@ -0,0 +1,31 @@
+def silhouette_diag(k,kmeans, X):
+    from sklearn.metrics import silhouette_samples, silhouette_score
+    from matplotlib.ticker import FixedLocator, FixedFormatter
+    import matplotlib as mpl
+    
+    
+
+    y_pred = kmeans.labels_
+    silhouette_coefficients = silhouette_samples(X, y_pred)
+
+    padding = len(X) // 30
+    pos = padding
+    ticks = []
+    for i in range(k):
+        coeffs = silhouette_coefficients[y_pred == i]
+        coeffs.sort()
+
+        color = mpl.cm.Spectral(i / k)
+        plt.fill_betweenx(np.arange(pos, pos + len(coeffs)), 0, coeffs,
+                          facecolor=color, edgecolor=color, alpha=0.9)
+        ticks.append(pos + len(coeffs) // 2)
+        pos += len(coeffs) + padding
+    plt.gca().yaxis.set_major_locator(FixedLocator(ticks))
+    plt.gca().yaxis.set_major_formatter(FixedFormatter(range(k)))
+    plt.ylabel("Cluster")
+    plt.gca().set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+    plt.xlabel("Silhouette Coefficient")
+    silhouette_score = silhouette_score(X,kmeans.labels_)
+    plt.axvline(x=silhouette_score, color="red", linestyle="--")
+    plt.title("$k={}$".format(k), fontsize=16)
+    plt.show()
