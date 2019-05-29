c2715d51301b0ff89496532fada765610f1ae40a
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-05-29 17:05:38 +0430

    Fix imports

--- src/clustering/veronoi.py
+++ src/clustering/veronoi.py
@@ -1,5 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_blobs
+
+
 def plot_data(X):
     plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
+
 
 def plot_centroids(centroids, weights=None, circle_color='w', cross_color='k'):
     if weights is not None:
@@ -10,7 +16,8 @@ def plot_centroids(centroids, weights=None, circle_color='w', cross_color='k'):
     plt.scatter(centroids[:, 0], centroids[:, 1],
                 marker='x', s=50, linewidths=50,
                 color=cross_color, zorder=11, alpha=1)
-    
+
+
 def plot_decision_boundaries(clusterer, X, resolution=1000, show_centroids=True,
                              show_xlabels=True, show_ylabels=True):
     mins = X.min(axis=0) - 0.1
@@ -21,7 +28,7 @@ def plot_decision_boundaries(clusterer, X, resolution=1000, show_centroids=True,
     Z = Z.reshape(xx.shape)
 
     plt.contourf(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
-                cmap="Pastel2")
+                 cmap="Pastel2")
     plt.contour(Z, extent=(mins[0], maxs[0], mins[1], maxs[1]),
                 linewidths=1, colors='k')
     plot_data(X)
