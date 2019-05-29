8b5166cf4c8cd546d4971fe3a4638d1f3a260f66
Kia Panahi Rad <kia.panahirad@gmail.com>
2019-05-29 19:16:27 +0430

    Add clustering with dbscan

---
+++ src/clustering/clustering_dbscan.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import make_moons
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+
+X, y = make_moons(n_samples=2000, random_state=42, noise=.1)
+
+dbscan = DBSCAN(eps=.1, min_samples=5)
+dbscan.fit(X)
+print('label: {}'.format(dbscan.labels_))
+
+outliers = X[dbscan.labels_ == -1]
+non_outliers = X[dbscan.labels_ > -1]
+
+plt.scatter(non_outliers[:, 0],
+            non_outliers[:, 1],
+            c=dbscan.labels_[dbscan.labels_ > -1])
+
+plt.scatter(outliers[:, 0],
+            outliers[:, 1],
+            marker='X',
+            s=50,
+            c='k')
+plt.show()
